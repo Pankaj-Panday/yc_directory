78a4c9a13c37a4ef8eeb2e69ccf78f2bb27897eb
Pankaj Panday <ppanday064@gmail.com>
2025-04-08 22:57:43 +0530

    fix: added webpack alias for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -14,14 +15,19 @@ const nextConfig: NextConfig = {
   experimental: {
     ppr: "incremental",
   },
-  devIndicators: {
-    position: "bottom-right",
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname),
+    };
+    return config;
   },
 };
 
