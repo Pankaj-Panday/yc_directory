d644124d45b0a7316545120068dac0ec47631f7c
Pankaj Panday <ppanday064@gmail.com>
2025-04-09 17:27:25 +0530

    live deploment fix

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,15 @@
 import SearchForm from "../../components/SearchForm";
 import StartupCard, { StartupCardType } from "@/components/StartupCard";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
-import { sanityFetch } from "@/lib/live";
 import { auth } from "@/auth";
+import { client } from "@/sanity/lib/client";
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
   const query = (await searchParams).query;
   const params = { search: query || null }
-  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
+  const { data: posts } = await client.fetch(STARTUPS_QUERY, params, {
+    next: { revalidate: 0 }, // or no-store if you're using fetch directly
+  });
 
   const session = await auth();
 
